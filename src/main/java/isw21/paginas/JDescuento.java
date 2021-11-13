f0c5d58ef5611328a30543c2aa1f17e4255ab1e3
MariaCosta1 <93608751+MariaCosta1@users.noreply.github.com>
2021-11-13 01:36:29 +0100

    Update JDescuento.java

--- src/main/java/isw21/paginas/JDescuento.java
+++ src/main/java/isw21/paginas/JDescuento.java
@@ -15,129 +15,68 @@ import org.apache.log4j.Logger;
 import main.java.isw21.client.Client;
 import main.java.isw21.descuentos.Descuento;
 
+
 public class JDescuento extends JFrame
 {
-    Customer customer;
+	Customer customer;
     Client cliente;
-    public JDescuento(Customer customer, Client cliente, ArrayList<Descuento> descuentos)
-    {
-        this.customer=customer;
+
+	public JDescuento(Customer customer, Client cliente, ArrayList<Descuento> descuentos)
+	{
+		this.customer=customer;
         this.cliente=cliente;
+        
+		setSize(450,300);
 
-        Font fuente = new Font("Serif", 0, 15);
-        Font fuente3 = new Font("Serif", 0, 18);
+		Font fuente = new Font("Serif", 0, 15);
+		Font fuente1 = new Font("Serif", 1, 18);
+		Font fuente2 = new Font("Serif", 0, 12);
+		Font fuente3 = new Font("Serif", 0, 30);
 
-        this.setLayout(new BorderLayout());
+		this.setLayout(new BorderLayout());
 
-        JPanel pnlNorte = new JPanel();
-        JLabel lblTitulo = new JLabel("Agregar nuevo descuento");
-        lblTitulo.setFont(fuente3);
-        pnlNorte.add(lblTitulo);
-
-        JPanel pnlSur = new JPanel();
-        JButton btnCrear = new JButton("Crear Descuento");
-        btnCrear.setFont(fuente);
-
-        JPanel pnlCentro = new JPanel();
-        pnlCentro.setLayout(new GridLayout(6,2));
-        //pnlCentro.setLayout(new FlowLayout());
-
-        JLabel lblEntidad = new JLabel("Entidad: ");
-        JTextField txtEntidad = new JTextField();
+	//NORTE
+		JPanel pnlNorte = new JPanel();
+		pnlNorte.setBackground(new Color(112,157,119));
+		JLabel lblTitulo = new JLabel("Agregar nuevo descuento");
+		lblTitulo.setFont(fuente3);
+		lblTitulo.setForeground(Color.white);
+		pnlNorte.setAlignmentX(lblTitulo.CENTER_ALIGNMENT);
+		pnlNorte.add(lblTitulo);
 
 
-/*Opción: Ponemos checkbox para indicar si tiene fecha de caducidad.
-Si se marca se abre un rellenable con fecha incio y fin. Esto
-facilita en el caso de agregar un calendario.
+	//CENTRO
 
-		JCheckBox chkCaducidad = new JCheckBox ("Marque si su descuento tiene caducidad");
-		this.add(chkCaducidad);
-		JLabel lbl = new JLabel();
-		this.add(lbl);
+		JPanel pnlCentro = new JPanel();
+		pnlCentro.setLayout(new GridLayout(6,2));
+		//pnlCentro.setLayout(new FlowLayout());
 
-		chkCaducidad.addActionListener(new ActionListener()
+		JLabel lblEntidad = new JLabel("Entidad: ");
+		JTextField txtEntidad = new JTextField();
+
+		JLabel lblInicio = new JLabel("Fecha inicio: ");
+		JTextField txtInicio = new JTextField();
+		JLabel lblFin = new JLabel("Fecha fin: ");
+		JTextField txtFin = new JTextField();
+
+		JLabel lblTipo = new JLabel("Tipo: ");
+		String[] tipos = {"Porcentaje", "Cantidad", "Cupon"};
+		JComboBox<String> cbxTipo = new JComboBox<String>(tipos);
+
+		JLabel lblValor = new JLabel("Valor: ");
+		JTextField txtValor = new JTextField();
+
+		JLabel lblCodigo = new JLabel("Codigo:");
+		JTextField txtCodigo = new JTextField();
+
+	//SUR
+		JPanel pnlSur = new JPanel();
+		JButton btnCrear = new JButton("Crear Descuento");
+		btnCrear.setForeground(new Color(37, 56, 40));
+		btnCrear.setFont(fuente);
+		btnCrear.addActionListener(new ActionListener()
 		{
-		   public void stateChanged(ChangeEvent e)
-		   {
-	        if (chkCaducidad.isSelected()==true)
-			}
-		});*/
-
-        JLabel lblInicio = new JLabel("Fecha inicio: ");
-        JTextField txtInicio = new JTextField();
-        JLabel lblFin = new JLabel("Fecha fin: ");
-        JTextField txtFin = new JTextField();
-
-        JLabel lblTipo = new JLabel("Tipo: ");
-        String[] tipos = {"Porcentaje", "Cantidad", "Cupon"};
-        JComboBox<String> cbxTipo = new JComboBox<String>(tipos);
-
-        JLabel lblValor = new JLabel("Valor: ");
-        JTextField txtValor = new JTextField();
-
-        JLabel lblCodigo = new JLabel("Codigo:");
-        JTextField txtCodigo = new JTextField();
-
-
-        txtEntidad.addKeyListener(new KeyAdapter()
-        {
-            @Override
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
-                    txtInicio.requestFocus();
-
-            }
-        });
-
-        txtInicio.addKeyListener(new KeyAdapter()
-        {
-            @Override
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
-                    txtFin.requestFocus();
-
-            }
-        });
-
-        txtFin.addKeyListener(new KeyAdapter()
-        {
-            @Override
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
-                    cbxTipo.requestFocus();
-
-            }
-        });
-
-        txtValor.addKeyListener(new KeyAdapter()
-        {
-            @Override
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
-                    txtCodigo.requestFocus();
-
-            }
-        });
-        txtCodigo.addKeyListener(new KeyAdapter()
-        {
-            @Override
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
-                    btnCrear.requestFocus();
-
-            }
-        });
-
-
-
-        btnCrear.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
+			public void actionPerformed(ActionEvent e)
             {
                 cliente.setContext("/addDescuento");
                 HashMap<String,Object> session= new HashMap<String,Object>();
@@ -150,30 +89,94 @@ facilita en el caso de agregar un calendario.
                 setVisible(false);
             }
 
-        });
-        pnlSur.add(btnCrear);
+		});
+		pnlSur.add(btnCrear);
 
+	//LADOS
+		JPanel pnlEste = new JPanel();
+		pnlEste.setPreferredSize(new Dimension(50, 60));
 
-        pnlCentro.add(lblEntidad);
-        pnlCentro.add(txtEntidad);
-        pnlCentro.add(lblInicio);
-        pnlCentro.add(txtInicio);
-        pnlCentro.add(lblFin);
-        pnlCentro.add(txtFin);
-        pnlCentro.add(lblTipo);
-        pnlCentro.add(cbxTipo);
-        pnlCentro.add(lblValor);
-        pnlCentro.add(txtValor);
-        pnlCentro.add(lblCodigo);
-        pnlCentro.add(txtCodigo);
+		JPanel pnlOeste = new JPanel();
+		pnlOeste.setPreferredSize(new Dimension(50, 60));
 
+//FUNCIONES
+		txtEntidad.addKeyListener(new KeyAdapter()
+		{
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+				if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
+					txtInicio.requestFocus();
+				
+			}
+		});
 
-        this.add(pnlCentro, BorderLayout.CENTER);
-        this.add(pnlNorte, BorderLayout.NORTH);
-        this.add(pnlSur, BorderLayout.SOUTH);
-        this.pack();
-        //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-        this.setLocation(550, 250);
-    }
+		txtInicio.addKeyListener(new KeyAdapter()
+		{
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+				if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
+					txtFin.requestFocus();
+			}
+		});
+
+		txtFin.addKeyListener(new KeyAdapter()
+		{
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+				if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
+					cbxTipo.requestFocus();
+				
+			}
+		});
+
+		txtValor.addKeyListener(new KeyAdapter()
+		{
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+				if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
+					txtCodigo.requestFocus();
+				
+			}
+		});
+		txtCodigo.addKeyListener(new KeyAdapter()
+		{
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+				if(e.getKeyCode()==KeyEvent.VK_ENTER) //valor de key: enter
+					btnCrear.requestFocus();
+				
+			}
+		});
+
+		pnlCentro.add(lblEntidad);
+		pnlCentro.add(txtEntidad);
+		pnlCentro.add(lblInicio);
+		pnlCentro.add(txtInicio);
+		pnlCentro.add(lblFin);
+		pnlCentro.add(txtFin);
+		pnlCentro.add(lblTipo);
+		pnlCentro.add(cbxTipo);
+		pnlCentro.add(lblValor);
+		pnlCentro.add(txtValor);
+		pnlCentro.add(lblCodigo);
+		pnlCentro.add(txtCodigo);
+		
+
+		this.add(pnlCentro, BorderLayout.CENTER);
+		this.add(pnlNorte, BorderLayout.NORTH);
+		this.add(pnlSur, BorderLayout.SOUTH);
+		this.add(pnlEste, BorderLayout.EAST);
+		this.add(pnlOeste, BorderLayout.WEST);
+		//this.pack();
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		this.setVisible(true); 
+		this.setLocation(550, 250);
+	}
+
+	
 }
